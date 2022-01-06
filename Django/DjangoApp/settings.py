4568ad797e50490a592f542b14db2238c4235105
mintropy <jun2korea@gmail.com>
2022-01-06 22:31:02 +0900

    django drf, cbv

--- Django/DjangoApp/settings.py
+++ Django/DjangoApp/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     
     'todo',
     
+    'rest_framework',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/todo/views.py
+++ Django/todo/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import get_list_or_404, render
+# from django.views.generic import ListView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
-class TodoView(ListView):
-    model = Todo
+class TodoView(APIView):
     
     def get(self, request):
-        response = JsonResponse(
-            self.model.objects.all()
-        )
-        return response
+        todos = get_list_or_404(Todo)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
