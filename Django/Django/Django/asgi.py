804e7d1cc8b33afdd9869cf45c16bfde2b29300a
mintropy <jun2korea@gmail.com>
2022-01-15 21:06:58 +0900

    new checkpoint

--- Django/Django/Django/asgi.py
+++ Django/Django/Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoApp project.
+ASGI config for Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django.settings')
 
 application = get_asgi_application()
--- Django/Django/Django/urls.py
+++ Django/Django/Django/urls.py
@@ -1,4 +1,4 @@
-"""DjangoApp URL Configuration
+"""Django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from todo.views import TodoView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('todo/', include('todo.urls')),
-    path('todo/', TodoView.as_view()),
 ]
