92278ca63dddeeab1238f1ec461a90da819397b3
mintropy <jun2korea@gmail.com>
2022-01-16 19:45:12 +0900

    strawberry-dajango guide

--- Django/fruit/schema.py
+++ Django/fruit/schema.py
@@ -1,11 +1,36 @@
+import random
 import strawberry
+import strawberry_django
+import strawberry_django.auth as auth
+from strawberry_django import mutations
 from typing import List
-from .types import Fruit
+from .types import (
+    Color, 
+    Fruit,
+    FruitFilter,
+    FruitInput
+)
+from . import models
+
+
+greetings = ["hi", "hello", "안녕"]
+def resolve_hello(root, info) -> str:
+    return random.choice(greetings)
 
 
 @strawberry.type
 class Query:
-    fruits: List[Fruit] = strawberry.django.field()
+    hello: str = strawberry.field(resolver=resolve_hello)
+    fruit: Fruit = strawberry_django.field()
+    fruits: List[Fruit] = strawberry_django.field(filters=FruitFilter)
+    # fruits: List[Fruit] = strawberry.field(resolver=resolve_fruit)
+    color: Color = strawberry_django.field()
+    colors: List[Color] = strawberry_django.field()
+
+
+@strawberry.type
+class Mutation:
+    createFruit: Fruit = mutations.create(FruitInput)
 
 
 schema = strawberry.Schema(query=Query)
--- Django/fruit/types.py
+++ Django/fruit/types.py
@@ -1,18 +1,71 @@
 import strawberry
+import strawberry_django
 from strawberry.django import auto
+from django.contrib.auth import get_user_model
 from typing import List
 from . import models
 
 
-@strawberry.django.type(models.Fruit)
+# filter
+# @strawberry.django.type(models.Fruit)
+@strawberry_django.filters.filter(models.Fruit, lookups=True)
+class FruitFilter:
+    id: auto
+    name: auto
+    color: 'ColorFilter'
+
+
+# @strawberry.django.type(models.Color)
+@strawberry_django.filters.filter(models.Color, lookups=True)
+class ColorFilter:
+    id: auto
+    name: auto
+    fruits: FruitFilter
+
+
+# order
+@strawberry_django.ordering.order(models.Fruit)
+class FruitOrder:
+    name: auto
+    color: 'ColorOrder'
+
+
+@strawberry_django.ordering.order(models.Color)
+class ColorOrder:
+    name: auto
+    fruit: FruitOrder
+
+
+# types
+@strawberry_django.type(
+    models.Fruit, filters=FruitFilter, order=FruitOrder, pagination=True
+)
 class Fruit:
     id: auto
     name: auto
     color: 'Color'
 
 
-@strawberry.django.type(models.Color)
+@strawberry_django.type(
+    models.Color, filters=ColorFilter, order=ColorOrder, pagination=True
+)
 class Color:
     id: auto
     name: auto
-    fruits: List[Fruit]
+    color: List[Fruit]
+
+
+@strawberry_django.type(get_user_model())
+class User:
+    id: auto
+    username: auto
+    password: auto
+    email: auto
+
+
+# input types
+@strawberry_django.input(models.Fruit)
+class FruitInput:
+    id: auto
+    name: auto
+    color: auto
