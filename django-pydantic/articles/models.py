1020b3bb57225439d04a9613bfab1963c63158bf
mintropy <jun2korea@gmail.com>
2022-02-24 12:57:27 +0900

    User Article Reply models

---
+++ django-pydantic/articles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Reply(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
