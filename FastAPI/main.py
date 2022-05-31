f8745bade91bb3115fb2c83d4fd355c20e11ff46
mintropy <jun2korea@gmail.com>
2022-05-31 11:00:48 +0900

    feat : fastapi main update

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,17 +1,28 @@
-"""Fast API main"""
-from typing import Optional
+from typing import Union
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 
-from .articles import router
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
-app.include_router(
-    router.article_router
-)
+
+@app.get("/")
+def get_item():
+    return {"Hello": "World"}
 
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'World'}
+@app.post("/")
+def create_item(item: Item, q: Union[str, None] = None):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
