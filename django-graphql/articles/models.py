52e19b11c095b761c3d0241500c5443ed232a55d
mintropy <jun2korea@gmail.com>
2022-06-01 10:37:11 +0900

    feat : django graphql models and url

--- django-graphql/articles/models.py
+++ django-graphql/articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Reply(models.Model):
+    article = models.ForeignKey(
+        Article,
+        related_name="replies",
+        on_delete=models.CASCADE,
+    )
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
