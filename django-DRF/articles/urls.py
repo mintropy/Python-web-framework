90fa525d5a343e427b78bba4cee1c0f803eb8808
Youngjun <jun2korea@gmail.com>
2022-07-23 13:57:18 +0900

    DRF article update & destroy article 수정, 삭제 코드 추가가

--- django-DRF/articles/urls.py
+++ django-DRF/articles/urls.py
@@ -3,16 +3,17 @@ from django.urls import path
 from .views.article import ArticleViewSet
 
 
-article_list = ArticleViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
-article_detail = ArticleViewSet.as_view({
-    'get': 'retrieve',
-})
+article_list = ArticleViewSet.as_view(
+    {
+        "get": "list",
+        "post": "create",
+    }
+)
+article_detail = ArticleViewSet.as_view(
+    {"get": "retrieve", "put": "update", "delete": "destroy"}
+)
 
 urlpatterns = [
-    path('', article_list),
-    path('<int:article_id>/', article_detail),
+    path("", article_list),
+    path("<int:article_id>/", article_detail),
 ]
-
